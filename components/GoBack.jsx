8d3c1fb617b052bc6164c76a75e4ca1fa2ffcc37
md5dalton <md5daltonggs@gmail.com>
2024-03-14 20:10:23 +0200

    Create GoBack.jsx

---
+++ components/GoBack.jsx
@@ -0,0 +1,19 @@
+"use client"
+import { usePathname, useRouter } from "next/navigation"
+
+export default () => {
+    const pathname = usePathname()
+    const router = useRouter()
+
+    const step = pathname.split("/").pop()
+
+    const handler = () => {
+
+        router.back() 
+        // push(`/step/${step}`)
+
+    }
+
+    
+    return <button onClick={handler} className={step < 2 ? "hidden" : ""}>Go Back</button>
+}
