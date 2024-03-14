eb449119b01d4bc610eb2e2e7f751100dc857c7c
md5dalton <md5daltonggs@gmail.com>
2024-03-14 20:58:26 +0200

    Update Footer.jsx

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -1,7 +1,9 @@
 import GoBack from "@/components/GoBack"
+import NextStep from "@/components/NextStep"
 
 export default () => (
-    <footer className="">
-        <GoBack />
+    <footer className="fixed w-full bottom-0 grid grid-cols-[auto_auto] items-center justify-between p-4 bg-white">
+        <div><GoBack /></div>
+        <div><NextStep /></div>
     </footer>
 )
