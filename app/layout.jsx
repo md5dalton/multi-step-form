e7b62098c6e17a8783c71e74f225a659b05f59a7
md5dalton <md5daltonggs@gmail.com>
2024-03-14 20:58:30 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,25 +4,33 @@ import "./globals.css"
 import Nav from "./Nav"
 
 const font = Ubuntu({ 
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "700"]
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "700"]
 })
 
 export const metadata = {
-  title: "Multi-step form",
-  description: "Multi-step form frontend mentor coding challenge",
+    title: "Multi-step form",
+    description: "Multi-step form frontend mentor coding challenge",
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en" className="bg-light-blue">
-      <body className={font.className}>
-        <Nav />
-        <main>
-          {children}
-        </main>
-        <Footer />
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en" className="bg-light-blue">
+            <body className={font.className}>
+                <Nav />
+                    <main
+                        style={{width: "calc(100% - 2rem)"}}
+                        className="
+                            absolute left-1/2 -translate-x-1/2 top-24
+                            px-6 py-8 shadow-card
+                            bg-white
+                            rounded-xl
+                        "
+                    >
+                        {children}
+                    </main>
+                <Footer />
+            </body>
+        </html>
+    )
 }
