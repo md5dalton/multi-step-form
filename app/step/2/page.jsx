3f6d033bf2f8b01b36b7c2e1a2be8e2d0f4d7744
md5dalton <md5daltonggs@gmail.com>
2024-03-24 11:31:59 +0200

    Update page.jsx

--- app/step/2/page.jsx
+++ app/step/2/page.jsx
@@ -1,17 +1,16 @@
 import UList from "@/components/UList"
-import Image from "next/image"
-import Fieldset from "./Fieldset"
+import Radio from "./Radio"
 
 const plans = {
     monthly: [
-        {name: "arcade", price: 9},
-        {name: "advanced", price: 12},
-        {name: "pro", price: 15},
+        {name: "arcade", price: "9/mo"},
+        {name: "advanced", price: "12/mo"},
+        {name: "pro", price: "15/mo"},
     ],
     yearly: [
-        {name: "arcade", price: 90, benefits: true},
-        {name: "advanced", price: 120, benefits: true},
-        {name: "pro", price: 150, benefits: true},
+        {name: "arcade", price: "90/yr", benefits: true},
+        {name: "advanced", price: "120/yr", benefits: true},
+        {name: "pro", price: "150/yr", benefits: true},
     ],
 }
 const plan ="yearly"
@@ -23,36 +22,21 @@ export default () => (
             <p className="text-cool-gray max-w-72">You have the option of monthly or yearly billing.</p>
         </div>
         <form className="grid gap-6">
-            <UList
-                className="grid gap-3"
-                items={plans.yearly}
-                itemHandler={({ name, price, benefits }) => (
-                    <fieldset className="border-[2px] border-light-gray rounded-lg overflow-hidden">
-                        <label htmlFor={name} className="flex gap-4 items-start p-4 cursor-pointer bg-very-light-gray">
-                            <Image
-                                height={40}
-                                width={40}
-                                alt="plan icon"
-                                src={`/icons/icon-${name}.svg`}
-                            />
-                            <div className="grid gap-1">
-                                <p className="capitalize font-medium">{name}</p>
-                                <p className="text-sm text-cool-gray">{`$${price}/${plan === "monthly" ? "mo" : "yr"}`}</p>
-                                {benefits && <p className="text-xs">2 months free</p>}
-                            </div>
-
-                        </label>
-                        <input className="hidden" type="radio" name={name} />
-                    </fieldset>
-                )}
-            />
-            <div className="flex items-center justify-center gap-6 capitalize text-sm h-12 bg-very-light-gray rounded-lg">
+            <fieldset>
+                <UList
+                    className="grid gap-3"
+                    items={plans.yearly}
+                    itemHandler={item => <Radio {...item} />
+                    }
+                />
+            </fieldset>
+            <fieldset className="flex items-center justify-center gap-6 capitalize text-sm h-12 bg-very-light-gray rounded-lg">
                 <p>monthly</p>
                 <div className="w-10 h-5 bg-marine-blue grid items-center px-1 rounded-xl">
                     <div className="h-3 w-3 rounded-full bg-white"></div>
                 </div>
                 <p>yearly</p>
-            </div>
+            </fieldset>
         </form>
     </div>
 )
